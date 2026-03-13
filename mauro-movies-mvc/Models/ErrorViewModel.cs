524e3cdff175a815fd0ac50b0d39a690d61dad22
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-03-13 02:32:11 -0300

    commit inicial

---
+++ mauro-movies-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mauro_movies_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
